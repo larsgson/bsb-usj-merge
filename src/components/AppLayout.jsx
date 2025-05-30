1af84d3500ca98da85e28eb5838895ae5d453f79
larsgson <4819617+larsgson@users.noreply.github.com>
2025-05-30 13:10:15 +0200

    improved string cleanup

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -19,6 +19,60 @@ export default function AppLayout() {
 
   const [result, setResult] = useState("");
 
+  const strip = (str) => str.trim()
+  
+  const cleanupString = (str) => {
+    console.log("'"+str+"'")
+    // const tempStr = str.replace(/ʼ/g, "’") // replace "ʼ" with "’"
+    // The above should hopefully be possible to handle - in reverse - from the generated usj from Martin Hosken
+    const tempStr = str.replace(/(\S+ )\s+/g, "$1") // remove multiple " "
+    const tempStr2 = tempStr.replace(/ \. /g, ". ") // replace " . " with ". "
+    const tempStr3 = tempStr2.replace(/ , /g, ", ") // replace " , " with ", "
+    const tempStr4 = tempStr3.replace(/([.|?|!]) ”/g, "$1”") // replace ". ”" with ".”"
+    const tempStr5 = tempStr4.replace(/, ”/g, ",”") // replace ". ”" with ".”"
+    const tempStr6 = tempStr5.replace(/“ (\S+)/g, "“$1") // replace "“ " with "“"
+    const tempStr7 = tempStr6.replace(/”(\S+)/g, "” $1") // replace "”" with "” "
+    // remove " " at the beginning (trim)
+    const tempStr8 = strip(tempStr7)
+    console.log("'"+tempStr8+"'")
+    return tempStr8
+  }
+
+  const cleanupItem = (item) => {
+    let resItem = item
+    console.log(item.constructor)
+    if (typeof item === 'string') {
+      resItem = cleanupString(item)
+    } else if (item.constructor === Array) {
+      console.log("arr")
+      resItem = cleanupArr(item)
+    } else if (item.constructor === Object) {
+      console.log("obj")
+      resItem = cleanupObj(item)
+    }
+    return resItem
+  }
+
+  const cleanupArr = (arr) => {
+    const result = arr.reduce((acc, item) => {
+      const useItem = cleanupItem(item)
+      return [ ...acc, useItem ]
+    }, [])
+    return result
+  }    
+
+  const cleanupObj = (obj) => {
+    const allKeys = Object.keys(obj)
+    const result = allKeys.reduce((acc, key) => {
+      let item = obj[key]
+      const useItem = cleanupItem(item)
+      return { ...acc, [key]: useItem }
+    }, {})
+    return result
+  }
+
+  const cleanedUpManualUSJ = cleanupObj(after)
+
   useEffect(() => {
     // console.log(diffString(before,after))
     // console.log(diff(before,after))
@@ -55,7 +109,7 @@ export default function AppLayout() {
       )}
         <JsonDiffComponent 
           jsonA={before}
-          jsonB={after}
+          jsonB={cleanedUpManualUSJ}
           styleCustomization={{
             additionLineStyle: { color: 'green' },
             deletionLineStyle: { color: 'red' },
